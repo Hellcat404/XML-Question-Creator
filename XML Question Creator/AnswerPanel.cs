96f0d23efd4718aeae6f15e64e6b09740f3fc3e0
Billy Jones <hellcat404@hotmail.com>
2020-09-08 18:23:41 +0100

    UI Functionality - Answer add/delete Question delete

---
+++ XML Question Creator/AnswerPanel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XML_Question_Creator
+{
+    public class AnswerPanel : Panel
+    {
+        Panel _apanel;
+        public int answer = 0;
+
+        Label answerlabel;
+
+        public AnswerPanel(Panel parent, int answers) {
+            answer = answers+1;
+            _apanel = parent;
+            parent.Size += new Size(0, 30);
+
+            this.Size = new Size(324, 30);
+            this.Location = new Point(0, (answers * 30) + 19);
+
+            answerlabel = new Label();
+            answerlabel.Text = String.Format("{0}:", (answers + 1).ToString());
+            answerlabel.Font = new Font(parent.Font.Name, parent.Font.Size, FontStyle.Regular);
+            answerlabel.Size = new Size(24, 13);
+            answerlabel.Location = new Point(0, 2);
+            this.Controls.Add(answerlabel);
+
+            TextBox answertextbox = new TextBox();
+            answertextbox.Font = new Font(parent.Font.Name, parent.Font.Size, FontStyle.Regular);
+            answertextbox.Size = new Size(201, 22);
+            answertextbox.Location = new Point(25, 0);
+            this.Controls.Add(answertextbox);
+
+            CheckBox answercheckbox = new CheckBox();
+            answercheckbox.Checked = false;
+            answercheckbox.Text = "correct?";
+            answercheckbox.Font = new Font(parent.Font.Name, parent.Font.Size, FontStyle.Regular);
+            answercheckbox.Size = new Size(66, 17);
+            answercheckbox.Location = new Point(232, 2);
+            this.Controls.Add(answercheckbox);
+
+            Button deleteanswerbutton = new Button();
+            deleteanswerbutton.Image = Properties.Resources.trash;
+            deleteanswerbutton.Size = new Size(24, 24);
+            deleteanswerbutton.Location = new Point(300, -1);
+            deleteanswerbutton.Click += new EventHandler(deleteAnswer);
+            this.Controls.Add(deleteanswerbutton);
+        }
+
+        private void deleteAnswer(object sender, EventArgs e) {
+            _apanel.Size -= new Size(0,30);
+            _apanel.Controls.Remove(this);
+            ((QuestionTab)_apanel.Parent).answerpanels.Remove(this);
+            ((QuestionTab)_apanel.Parent).UpdateAnswers();
+            this.Dispose();
+        }
+
+        public void UpdateAnswer() {
+            answerlabel.Text = String.Format("{0}:", (answer + 1).ToString());
+            this.Location = new Point(0, (answer * 30) + 19);
+        }
+    }
+}
--- XML Question Creator/QCreator.cs
+++ XML Question Creator/QCreator.cs
@@ -21,161 +21,32 @@ namespace XML_Question_Creator
         }
 
         private void Initialize() { 
-            questions = new List<Question>();
+            if(questions == null)
+                questions = new List<Question>();
+            questions.Clear();
             tctlQuestions.TabPages.Clear();
             tctlQuestions.TabPages.Add("Add Question");
         }
 
         private void AddQuestion() {
             questions.Add(new Question(String.Format("Question {0}", questions.Count), null, null));
-            CreateQuestionTab(String.Format("Question {0}",questions.Count));
+            CreateQuestionTab(questions.Count);
         }
 
-        private void CreateQuestionTab(string name) {
-            tctlQuestions.TabPages.Insert(tctlQuestions.TabCount - 1, name);
-            TabPage tab = tctlQuestions.TabPages[tctlQuestions.TabCount - 2];
-
-            Label qtextlbl = new Label();
-            qtextlbl.Name = "lblQText";
-            qtextlbl.Text = "Question Text";
-            qtextlbl.Font = new Font(tab.Font.Name,tab.Font.Size,FontStyle.Bold);
-            qtextlbl.Size = new Size(78,13);
-            qtextlbl.Location = new Point(8,13);
-            tab.Controls.Add(qtextlbl);
-
-            TextBox qtextbox = new TextBox();
-            qtextbox.Name = "txtQText";
-            qtextbox.Font = new Font(tab.Font.Name, tab.Font.Size, FontStyle.Regular);
-            qtextbox.Multiline = true;
-            qtextbox.Size = new Size(271,72);
-            qtextbox.Location = new Point(8,29);
-            tab.Controls.Add(qtextbox);
-
-            Label qimagelbl = new Label();
-            qimagelbl.Name = "lblQImage";
-            qimagelbl.Text = "Question Image (Path)";
-            qimagelbl.Font = new Font(tab.Font.Name, tab.Font.Size, FontStyle.Bold);
-            qimagelbl.Size = new Size(124,13);
-            qimagelbl.Location = new Point(8,114);
-            tab.Controls.Add(qimagelbl);
-
-            TextBox qimagetextbox = new TextBox();
-            qimagetextbox.Name = "txtQImage";
-            qimagetextbox.Font = new Font(tab.Font.Name, tab.Font.Size, FontStyle.Regular);
-            qimagetextbox.Size = new Size(187,22);
-            qimagetextbox.Location = new Point(11,130);
-            tab.Controls.Add(qimagetextbox);
-
-            Button browsebutton = new Button();
-            browsebutton.Name = "btnBrowse";
-            browsebutton.Text = "Browse...";
-            browsebutton.Font = new Font(tab.Font.Name, tab.Font.Size, FontStyle.Regular);
-            browsebutton.Size = new Size(75,24);
-            browsebutton.Location = new Point(204,129);
-            browsebutton.Click += new EventHandler(btnBrowse_Click);
-            tab.Controls.Add(browsebutton);
-
-            PictureBox qimagepicturebox = new PictureBox();
-            qimagepicturebox.Name = "pbQImage";
-            qimagepicturebox.Size = new Size(90,90);
-            qimagepicturebox.Location = new Point(11,156);
-            tab.Controls.Add(qimagepicturebox);
-
-            Button removebutton = new Button();
-            removebutton.Name = "btnQRemove";
-            removebutton.Text = "Remove Question";
-            removebutton.Font = new Font(tab.Font.Name, tab.Font.Size, FontStyle.Regular);
-            removebutton.Size = new Size(121,23);
-            removebutton.Location = new Point(11,268);
-            removebutton.Click += new EventHandler(btnQRemove_Click);
-            tab.Controls.Add(removebutton);
-
-            Panel answerspanel = new Panel();
-            answerspanel.Name = "panAnswers";
-            answerspanel.Font = new Font(tab.Font.Name, tab.Font.Size, FontStyle.Regular);
-            answerspanel.Size = new Size(305,69);
-            answerspanel.Location = new Point(324,13);
-            tab.Controls.Add(answerspanel);
-
-            Label answerslabel = new Label();
-            answerslabel.Name = "lblAnswers";
-            answerslabel.Text = "Answers";
-            answerslabel.Font = new Font(tab.Font.Name, tab.Font.Size, FontStyle.Bold);
-            answerslabel.Size = new Size(51,13);
-            answerslabel.Location = new Point(131,0);
-            answerspanel.Controls.Add(answerslabel);
-
-            Label answerlabel = new Label();
-            answerlabel.Text = "1:";
-            answerlabel.Font = new Font(tab.Font.Name, tab.Font.Size, FontStyle.Regular);
-            answerlabel.Size = new Size(16,13);
-            answerlabel.Location = new Point(3,19);
-            answerspanel.Controls.Add(answerlabel);
-
-            TextBox answertextbox = new TextBox();
-            answertextbox.Font = new Font(tab.Font.Name, tab.Font.Size, FontStyle.Regular);
-            answertextbox.Size = new Size(201,22);
-            answertextbox.Location = new Point(25,16);
-            answerspanel.Controls.Add(answertextbox);
-
-            CheckBox answercheckbox = new CheckBox();
-            answercheckbox.Text = "correct?";
-            answercheckbox.Font = new Font(tab.Font.Name, tab.Font.Size, FontStyle.Regular);
-            answercheckbox.Size = new Size(66,17);
-            answercheckbox.Location = new Point(232,19);
-            answerspanel.Controls.Add(answercheckbox);
-
-            Button addanswerbutton = new Button();
-            addanswerbutton.Name = "btnAddAnswer";
-            addanswerbutton.Text = "Add Answer";
-            addanswerbutton.Anchor = AnchorStyles.Bottom;
-            addanswerbutton.Size = new Size(292,23);
-            addanswerbutton.Location = new Point(6,43);
-            addanswerbutton.Click += new EventHandler(btnAddAnswer_Click);
-            answerspanel.Controls.Add(addanswerbutton);
-
+        private void CreateQuestionTab(int question) {
+            tctlQuestions.TabPages.Insert(tctlQuestions.TabCount - 1, new QuestionTab(this, question));
             tctlQuestions.SelectedIndex = tctlQuestions.TabCount - 2;
         }
 
-        private void btnAddAnswer_Click(object sender, EventArgs e) {
-            Panel parent = (Panel)((Button)sender).Parent;
-            Label answerlabel = new Label();
-            answerlabel.Text = "1:";
-            answerlabel.Font = new Font(parent.Font.Name, parent.Font.Size, FontStyle.Regular);
-            answerlabel.Size = new Size(16, 13);
-            answerlabel.Location = new Point(3, 19);
-            parent.Controls.Add(answerlabel);
-
-            TextBox answertextbox = new TextBox();
-            answertextbox.Font = new Font(parent.Font.Name, parent.Font.Size, FontStyle.Regular);
-            answertextbox.Size = new Size(201, 22);
-            answertextbox.Location = new Point(25, 16);
-            parent.Controls.Add(answertextbox);
-
-            CheckBox answercheckbox = new CheckBox();
-            answercheckbox.Text = "correct?";
-            answercheckbox.Font = new Font(parent.Font.Name, parent.Font.Size, FontStyle.Regular);
-            answercheckbox.Size = new Size(66, 17);
-            answercheckbox.Location = new Point(232, 19);
-            parent.Controls.Add(answercheckbox);
-        }
-
-        private void btnBrowse_Click(object sender, EventArgs e) {
-
-        }
-
-        private void btnQRemove_Click(object sender, EventArgs e) {
-
-        }
-
-        private void RemoveQuestion(int i) {
+        public void RemoveQuestion(int i) {
             questions.RemoveAt(i);
             NumberTabs();
         }
 
         private void NumberTabs() {
             for (int i = 0; i < questions.Count; i++) {
-                tctlQuestions.TabPages[i].Text = String.Format("Question {0}", i+1);
+                ((QuestionTab)tctlQuestions.TabPages[i]).question = i+1;
+                ((QuestionTab)tctlQuestions.TabPages[i]).UpdateQuestion();
             }
         }
 
