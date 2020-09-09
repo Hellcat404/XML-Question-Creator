506b1bf2ef3b09e12e9ea0eb067542ff6634ed50
Billy Jones <hellcat404@hotmail.com>
2020-09-09 05:36:46 +0100

    All main functionality complete (Export XML & Images)

--- XML Question Creator/QCreator.cs
+++ XML Question Creator/QCreator.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 using XML_Question_Creator.Models;
 
 namespace XML_Question_Creator
@@ -71,7 +72,7 @@ namespace XML_Question_Creator
             if(fd.ShowDialog() == DialogResult.OK) {
                 string apath = fd.SelectedPath;
                 string dpath = apath + @"\app\src\main\res\drawable\";
-                string xpath = apath + @"\app\src\main\res\xml";
+                string xpath = apath + @"\app\src\main\res\xml\";
 
                 for (int i = 0; i < questions.Count; i++) {
                     questions[i].setText(((QuestionTab)tctlQuestions.TabPages[i])._text);
@@ -79,9 +80,30 @@ namespace XML_Question_Creator
                     questions[i].setAnswers(((QuestionTab)tctlQuestions.TabPages[i]).getAnswers());
                 }
                 for (int i = 0; i < questions.Count; i++) {
-                    questions[i].getImage().Save(String.Format("{0}Q{1}.png",dpath , i),System.Drawing.Imaging.ImageFormat.Png);
+                    questions[i].getImage().Save(String.Format("{0}q{1}.png",dpath , i),System.Drawing.Imaging.ImageFormat.Png);
+                }
+                createQuestionsXML().Save(String.Format("{0}questionxml.xml",xpath));
+            }
+        }
+
+        private XDocument createQuestionsXML() {
+            XDocument qxml = new XDocument();
+            XElement root = new XElement("questions");
+            qxml.Add(root);
+            for (int i = 0; i < questions.Count; i++) {
+                XElement q = new XElement("question");
+                root.Add(q);
+                q.Add(new XAttribute("text", questions[i].getText()));
+                q.Add(new XAttribute("image", String.Format("q{0}",i)));
+                List<Answer> qans = questions[i].getAnswers();
+                for (int j = 0; j < qans.Count; j++) {
+                    XElement a = new XElement("answer");
+                    q.Add(a);
+                    a.Add(new XAttribute("text", qans[j].getText()));
+                    a.Add(new XAttribute("correct", qans[j].isCorrect()));
                 }
             }
+            return qxml;
         }
     }
 }
